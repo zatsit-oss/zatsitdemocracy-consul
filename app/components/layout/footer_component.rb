cd8200473cd6b14e72b78c23c3de39e237cf054d
Ludovic Dussart <ludovic.dussart.pro@gmail.com>
2025-11-21 11:29:32 +0100

    feat: add app version in footer

--- app/components/layout/footer_component.rb
+++ app/components/layout/footer_component.rb
@@ -3,6 +3,17 @@ class Layout::FooterComponent < ApplicationComponent
     !Rails.application.multitenancy_management_mode?
   end
 
+  def version
+    publiccode_path = Rails.root.join('publiccode.yml')
+    if File.exist?(publiccode_path)
+      require 'yaml'
+      yaml = YAML.load_file(publiccode_path)
+      yaml['softwareVersion'] || 'unknown'
+    else
+      'unknown'
+    end
+  end
+
   private
 
     def open_source_link
