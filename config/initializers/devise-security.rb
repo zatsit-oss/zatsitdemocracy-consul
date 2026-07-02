e0b4b4b53da076e1171c730e727db296c1742484
Ludovic Dussart <ludovic.dussart.pro@gmail.com>
2026-07-02 12:14:51 +0200

    feat(config): set expire.password to 10.years

--- config/initializers/devise-security.rb
+++ config/initializers/devise-security.rb
@@ -3,7 +3,7 @@ Devise.setup do |config|
   # Configure security extension for devise
 
   # Should the password expire (e.g 3.months)
-  config.expire_password_after = 1.year
+  config.expire_password_after = 10.year
 
   # Need 1 char each of: A-Z, a-z, 0-9, and a punctuation mark or symbol
   # You may use "digits" in place of "digit" and "symbols" in place of
